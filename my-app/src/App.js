833531f664210cf245dd0e83146c9d0bc07519a9
BluisHMoon <wes1619@hotmail.com>
2021-07-24 00:59:05 +0800

    簡易欄位建置，已可顯示在下方

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,57 +1,110 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="content">
-      <div className="list">
-        <div className="input-area">
-          標題輸入框
-          <input 
-            type="text" 
-            name="title"
-          />
-        </div>
-        <div className="input-area">
-          內容輸入框
-          <input 
-            type="text" 
-            name="description"
-          />
-        </div>
-        <div className="btn-area">
-          <button 
-            type="button"
-            name="submit"
-            onClick={insertNewData}
-          >
-            新增
-          </button>
-        </div>
-      </div>
-      <div className="result_area">
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { items: [], title: '', description: ''};
+    this.titleChange = this.titleChange.bind(this);
+    this.descriptionChange = this.descriptionChange.bind(this);
+    this.dataSubmit = this.dataSubmit.bind(this);
+  }
 
+  // titleChange = () => {
+  //   const {} = this.props;
+  // }
+
+  // descriptionChange = () => {
+  //   const {} = this.props;
+  // }
+
+  // dataSubmit = () => {
+  //   const {} = this.props;
+  // }
+
+  render(){
+    return(
+      <div className="content">
+        <div className="list">
+          <div className="input-area">
+            標題輸入框
+            <input 
+              type="text" 
+              name="title"
+              onChange={this.titleChange}
+              value={this.state.title}
+            />
+          </div>
+          <div className="input-area">
+            內容輸入框
+            <input 
+              type="text" 
+              name="description"
+              onChange={this.descriptionChange}
+              value={this.state.description}
+            />
+          </div>
+          <div className="btn-area">
+            <button 
+              type="button"
+              name="submit"
+              onClick={this.dataSubmit}
+            >
+              新增
+            </button>
+          </div>
+        </div>
+        <div className="result_area">
+          <TODOResult items = {this.state.items} />
+        </div>
       </div>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+    );
+  }
+
+  titleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  descriptionChange(e) {
+    this.setState({ description: e.target.value });
+  }
+
+  dataSubmit(e){
+    e.preventDefault();
+    if(this.state.title.length === 0 || this.state.description.length === 0){
+      return;
+    }
+
+    const newItem = {
+      id: Date.now(),
+      title: this.state.title,
+      description: this.state.description
+    };
+
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      title: '',
+      description: ''
+    }));
+  }
 }
 
+class TODOResult extends React.Component{
+  render(){
+    return(
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>
+            <div>{item.title}</div>
+            <div>{item.description}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 
 export default App;
