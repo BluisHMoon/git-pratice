6ef38e02e75e4b2b756d47b03155ed4ba82656ef
BluisHMoon <wes1619@hotmail.com>
2021-08-05 01:59:15 +0800

    Recreate TODOList and completed task 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
+//import ReactDOM from 'react-dom';
+//import logo from './logo.svg';
 import './App.css';
 import TODOForm from './view/TODOForm';
 import TODOList from './view/TODOList';
@@ -9,22 +9,22 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      list : [],
+      lists : [],
     }
   }
 
   handleSubmitInput(data){
-    this.state.list.push(data);
+    this.state.lists.push(data);
     this.setState({
-      list: this.state.list
+      lists: this.state.lists
     });
   }
 
   render(){
     return(
       <div className="content">
-        <TODOForm onClick={this.handleSubmitInput.bind(this)}/>
-        <TODOList/>
+        <TODOForm submitTODO={this.handleSubmitInput.bind(this)}/>
+        <TODOList lists={this.state.lists}/>
       </div>
     );
   }
